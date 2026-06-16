042cef7d5353c33ec47e645077da61f08a90f9c9
Muhammad ijaz <75518471+Muhammad-ijazCRT@users.noreply.github.com>
2026-06-16 10:24:16 +0500

    asdfasdf

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-J4XVEEMHMQ" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-J4XVEEMHMQ');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
